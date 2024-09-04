dea2ac07988a7c52938ce2a0f55df3defe1079f4
jeon <jeon@i-on.net>
2024-09-04 17:14:47 +0900

    room type 추가, 그룹 채팅일 경우 인원수 제한 입력, 타입 및 채팅방 이름 입력 분기 처리

--- .github/1_socket/components/CreateModal.vue
+++ .github/1_socket/components/CreateModal.vue
@@ -4,6 +4,16 @@
             <div class="modal-content" @click.stop>
                 <button class="close-button" @click="handleClose">X</button>
                 <h2>채팅 방 생성</h2>
+                <div v-for="type in roomType" :key="type.name">
+                    <input
+                        type="radio"
+                        name="roomType"
+                        :value="type.name"
+                        v-model="createRoomType"
+                    />
+                    {{ type.name }}
+                </div>
+                <input v-if="createRoomType === '그룹'" placeholder="인원 수" v-model="peopleCount"><br/>
                 <input v-model="newRoomName" placeholder="채팅 방 이름을 입력하세요"/>
                 <button @click="createRoom">생성</button>
             </div>
@@ -12,24 +22,42 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import { defineProps, defineEmits } from 'vue';
 
 const props = defineProps({
     isVisible: Boolean,
 });
 
+const roomType = ref([
+    {name : '일반'},
+    {name : '그룹'}
+])
+const createRoomType = ref('')
 const newRoomName = ref('')
+const peopleCount = ref('')
 const emit = defineEmits(['close', 'create-room']);
 
 const createRoom = () => {
-    if(newRoomName.value){
-        emit('create-room',newRoomName.value)
-        newRoomName.value = ''
+    if(createRoomType.value){
+        if(newRoomName.value){
+            emit('create-room',{name : newRoomName.value, type : createRoomType.value, cnt : peopleCount.value})
+            console.log(createRoomType.value)
+            newRoomName.value = ''
+            createRoomType.value = ''
+        } else {
+            alert('채팅방 이름을 입력 하세요.')
+        }
+    } else {
+        alert('채팅방 타입을 선택 하세요.')
     }
+
 }
 
 const handleClose = () => {
     emit('close');
+    newRoomName.value = ''
+    createRoomType.value = ''
 };
 </script>
 
