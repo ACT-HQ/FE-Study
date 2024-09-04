0c2f3692efe32832de83218ff05dec38518d4ee6
jeon <jeon@i-on.net>
2024-09-04 17:13:03 +0900

    생성 채팅 id, name, type, 인원 수 제한 전달 추가

--- .github/1_socket/index.vue
+++ .github/1_socket/index.vue
@@ -1,13 +1,12 @@
 <template>
     <h1>채팅방</h1>
-    <button @click="openModal">+</button>
-    <ul>
-        <li v-for="room in rooms" :key="room.name">
-            <div>
-                <nuxt-link :to="`/room/${room.name}`">{{ room.name }}</nuxt-link>
-            </div>
+    <button @click="openModal">채팅방 생성</button>
+    <ul v-for="room in rooms" :key="room.id">
+        <li v-if="room.name !== ''">
+            <nuxt-link :to="{ path: `/room/${room.name}`, query: { id: room.id, name:room.name, type: room.type } }">{{ room.name }} / {{ room.type }} 채팅 <span v-if="room.cnt"> / {{ room.cnt}}명 - 제한 </span></nuxt-link>
         </li>
     </ul>
+
     <!-- emit(create-room) 으로 component에서 입력된 input값 전달   -->
     <CreateModal :isVisible="isModalVisible"  @close="closeModal" @create-room="createRoom" />
 </template>
@@ -17,7 +16,7 @@ import CreateModal from "~/components/CreateModal.vue";
 
 const isModalVisible = ref(false)
 const rooms = ref([
-    {id : '', name : ''}
+    {id : '', name : '', type : '',  cnt : ''}
 ])
 
 function openModal(){
@@ -27,10 +26,11 @@ function closeModal(){
     isModalVisible.value = false
 }
 
-function createRoom(roomName){
+function createRoom(roomData){
     closeModal()
     const newId = rooms.value.length + 1
-    rooms.value.push({id: newId, name: `${roomName}`})
+    rooms.value.push({id: newId, name: roomData.name, type : roomData.type, cnt : roomData.cnt })
 }
 
+// 그룹 채팅 시 인원수 제한 추가 예정
 </script>
