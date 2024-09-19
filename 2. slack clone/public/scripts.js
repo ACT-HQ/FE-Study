f9658c2f78134292ae688a3268234557fcf24923
sjuhan123 <sjuhan123@gmail.com>
2024-09-19 16:15:18 +0900

    [slack clone] prevent duplicate socket connections

--- 2. slack clone/public/scripts.js
+++ 2. slack clone/public/scripts.js
@@ -4,6 +4,21 @@
   */
 const socket = io('http://localhost:9000');
 
+const nameSpaceSockets = [];
+const listeners = {
+    nsChange: [],
+}
+
+const addListeners = (nsId) => {
+    if(!listeners.nsChange[nsId]){
+        nameSpaceSockets[nsId].on('nsChange', (data) => {
+            console.log('Namespace changed');
+            console.log(data);
+        })
+        listeners.nsChange[nsId] = true;
+    }
+};
+
 socket.on('connect', (message) => {
     console.log('connected!!');
     socket.emit('clientConnected');
@@ -17,12 +32,13 @@ socket.on('nsList', (nsData) => {
     nsData.forEach((ns) => {
         nameSpacesDiv.innerHTML += `<div class="namespace" ns="${ns.endpoint}"><img src="${ns.image}"></div>`;
         // io()를 이용해 ns에 동적으로 join한다.
-        const thisNs = io(`http://localhost:9000${ns.endpoint}`);
+        //const thisNs = io(`http://localhost:9000${ns.endpoint}`);
         // 서버에서 namespace 정보가 바뀌면(nsChange 이벤트가 emit되면), 자동으로 client에도 변경사항이 전달된다.
-        thisNs.on('nsChange', (data) => {
-            console.log('Namespace changed');
-            console.log(data);
-        })
+
+        if(!nameSpaceSockets[ns.id]){
+            nameSpaceSockets[ns.id] = io(`http://localhost:9000${ns.endpoint}`);
+        }
+        addListeners(ns.id);
     })
 
     /**
