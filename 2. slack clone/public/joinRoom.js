db8f8aec10bd3710d407e19c640c618803d32fef
sjuhan123 <sjuhan123@gmail.com>
2024-09-19 17:20:03 +0900

    [slack clone] refactor joinRoom
    - migrate callback to async-await by using emitWithAck

--- 2. slack clone/public/joinRoom.js
+++ 2. slack clone/public/joinRoom.js
@@ -1,11 +1,11 @@
-const joinRoom = (roomTitle, namespaceId) => {
+const joinRoom = async (roomTitle, namespaceId) => {
     console.log('roomTitle', roomTitle);
     console.log('namespaceId', namespaceId);
-    nameSpaceSockets[namespaceId].emit('joinRoom', roomTitle, (ackResp) => {
-        const numUsersDom = document.querySelector('.curr-room-num-users');
-        numUsersDom.innerHTML = `${ackResp.numUsers}<span class="fa-solid fa-user"></span>`
+    const ackResp = await nameSpaceSockets[namespaceId].emitWithAck('joinRoom', roomTitle);
 
-        const roomTitleDom = document.querySelector('.curr-room-text');
-        roomTitleDom.innerHTML = roomTitle;
-    });
+    const numUsersDom = document.querySelector('.curr-room-num-users');
+    numUsersDom.innerHTML = `${ackResp.numUsers}<span class="fa-solid fa-user"></span>`
+
+    const roomTitleDom = document.querySelector('.curr-room-text');
+    roomTitleDom.innerHTML = roomTitle;
 };
